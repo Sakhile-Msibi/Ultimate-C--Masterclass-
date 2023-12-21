4c76e56d499b2add2d667ebcc950afdfc51c6e08
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2023-12-21 10:26:39 +0200

    Ultimate C# Masterclass

---
+++ code/Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            string userInput;
+
+            Console.WriteLine("Hello!\nInput the first number:");
+
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the first number:");
+
+            secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What do you want to do with those numbers?");
+            Console.WriteLine("[A]dd\n[S]ubtract\n[M]ultiply");
+
+            userInput = Console.ReadLine();
+
+            if (userInput == "a" || userInput == "A")
+            {
+                Console.WriteLine(firstNumber + "  + " + secondNumber + " = " + (firstNumber + secondNumber));
+            }
+            else if (userInput == "s" || userInput == "S")
+            {
+                Console.WriteLine(firstNumber + "  - " + secondNumber + " = " + (firstNumber - secondNumber));
+            }
+            else if (userInput == "m" || userInput == "M")
+            {
+                Console.WriteLine(firstNumber + "  * " + secondNumber + " = " + (firstNumber * secondNumber));
+            }
+            else
+            {
+                Console.WriteLine("Invalid option");
+            }
+
+            Console.WriteLine("Press any key to close");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ code/TodoList/Program.cs
@@ -0,0 +1,163 @@
+﻿namespace TodoList
+{
+    internal class Program
+    {
+        List<string> ToDoList = new List<string>();
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            string? userInput = "";
+            bool isUserIputInvalid = false;
+
+            Console.WriteLine("Hello!\n");
+
+            do
+            {
+                isUserIputInvalid = true;
+
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("[S]ee all TODOs");
+                Console.WriteLine("[A]dd a TODO");
+                Console.WriteLine("[R]emove a TODO");
+                Console.WriteLine("[E]xit");
+
+                userInput = Console.ReadLine();
+
+                if (userInput != null)
+                {
+                    userInput = userInput.ToUpper();
+                }
+
+                switch (userInput)
+                {
+                    case "S":
+                        program.DisplayToDoList();
+                        break;
+                    case "A":
+                        isUserIputInvalid = program.AddToDo();
+                        break;
+                    case "R":
+                        isUserIputInvalid = program.RemoveATODO();
+                        break;
+                    case "E":
+                        isUserIputInvalid = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice\n");
+                        break;
+                }
+            } while (isUserIputInvalid);
+        }
+
+        bool RemoveATODO()
+        {
+            bool isIndexValid = false;
+
+            if (ToDoList.Count == 0) 
+            {
+                Console.WriteLine("No TODOs have been added yet\n");
+                return true;
+            }
+
+            do
+            {
+                Console.WriteLine("Select the index of the TODO you want to remove:");
+                DisplayToDoList();
+
+                string? indexToRemove = Console.ReadLine();
+
+                if (indexToRemove != null)
+                {
+                    if (indexToRemove.Length == 0)
+                    {
+                        Console.WriteLine("Selected index cannot be empty.");
+                        isIndexValid = false;
+                    }
+                    else if (int.TryParse(indexToRemove, out int toDoIndex) && (toDoIndex > 0 && toDoIndex <= ToDoList.Count))
+                    {
+                        string removedTODO = ToDoList[toDoIndex - 1];
+                        ToDoList.Remove(ToDoList[toDoIndex - 1]);
+                        Console.WriteLine("TODO removed: " + removedTODO);
+                        Console.WriteLine();
+                        isIndexValid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The given index is not valid.");
+                        isIndexValid = false;
+                    }
+                }
+               
+            } while (!isIndexValid);
+
+            return isIndexValid;
+        }
+
+        void DisplayToDoList()
+        {
+            if (ToDoList.Count > 0)
+            {
+                for (int i = 0; i < ToDoList.Count; i++)
+                {
+                    Console.WriteLine($"{(i + 1)}. {ToDoList[i]}");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("No TODOs have been added yet\n");
+            }
+        }
+
+        bool AddToDo()
+        {
+            bool isToDoValid = false;
+
+            do
+            {
+                isToDoValid = true;
+                Console.WriteLine("Enter the TODO description:");
+                string? tODOdescription = Console.ReadLine();
+
+                if (tODOdescription != null)
+                {
+                    if (tODOdescription.Length > 0)
+                    {
+                        foreach (string value in ToDoList)
+                        {
+                            if (value.Equals(tODOdescription))
+                            {
+                                isToDoValid = false;
+                                break;
+                            }
+                        }
+
+                        if (isToDoValid)
+                        {
+                            ToDoList.Add(tODOdescription);
+
+                            Console.WriteLine("TODO successfully added: " + ToDoList[^1]);
+                            Console.WriteLine();
+                            isToDoValid = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The description must be unique.");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("The description cannot be empty.");
+                        isToDoValid = false;
+                    }
+                }
+
+            } while (!isToDoValid);
+
+            return isToDoValid;
+        }
+    }
+}
