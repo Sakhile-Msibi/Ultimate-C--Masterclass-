bbb2f731d88c8b9e2a15665494b95f3efa770885
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2024-01-10 05:11:45 +0200

    Dice Roll Game

---
+++ code/DiceRollGame/DiplayMessage.cs
@@ -0,0 +1,38 @@
+﻿namespace DiceRollGame
+{
+    public class DisplayMessage
+    {
+        public void StartMessage()
+        {
+            Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
+            EnterNumberMessage();
+        }
+
+        public void WrongNumberMessage()
+        {
+            Console.WriteLine("Wrong number");
+            EnterNumberMessage();
+        }
+
+        public void IncorrectNumberMessage()
+        {
+            Console.WriteLine("Incorrect input");
+            EnterNumberMessage();
+        }
+
+        public void EnterNumberMessage()
+        {
+            Console.WriteLine("Enter a number:");
+        }
+
+        public void LossMessage()
+        {
+            Console.WriteLine("You lose :(");
+        }
+
+        public void WinMessage()
+        {
+            Console.WriteLine("You win! :)");
+        }
+    }
+}
--- code/DiceRollGame/Program.cs
+++ code/DiceRollGame/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace DiceRollGame
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            RollDice _dice = new RollDice();
+            EnterNumber _enterNumber = new EnterNumber();
+            GameResult _gameResult = new GameResult();
+            GameLoss _gameLoss = new GameLoss();
+            DisplayMessage displayMessage = new DisplayMessage();
+
+            const int MAXGUESSES = 3;
+            int numberOfGuesses = 0;
+            int rolledNumber = _dice.Roll();
+
+            displayMessage.StartMessage();
+
+            do
+            {
+                int number = _enterNumber.EnterNumberFromConsole();
+
+                if (!_gameResult.GameResults(rolledNumber, number))
+                {
+                    numberOfGuesses++;
+                }
+                else
+                {
+                    break;
+                }
+
+                _gameLoss.output(numberOfGuesses, MAXGUESSES);
+
+            } while (numberOfGuesses != MAXGUESSES);
+
+            Console.ReadKey();
         }
     }
 }
