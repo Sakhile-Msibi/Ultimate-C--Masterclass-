7bf0641bbec9ffb22ca605a28df551e339fb1d21
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2024-03-12 05:03:24 +0200

    New game data parser skeleton

---
+++ code/GameDataParser/FileValidation.cs
@@ -0,0 +1,15 @@
+﻿namespace GameDataParser
+{
+    public class FileValidation
+    {
+        public static bool DoesFileExist(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ code/GameDataParser/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml.Linq;
+
+namespace GameDataParser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+
+            Console.ReadKey();
+        }
+
+        public static void App()
+        {
+            bool isNameValid = false;
+            bool isFileDataValid = false;
+            bool isJsonFileNotEmpty = false;
+            do
+            {
+                Console.WriteLine("Enter the name of the file you want to read:");
+
+                string? fileNameEnteredByAUser = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(fileNameEnteredByAUser))
+                {
+                    isNameValid = DoesFileExist(fileNameEnteredByAUser);
+
+                    if (isNameValid)
+                    {
+                        isFileDataValid = IsFileDataValid();
+                    }
+                    else
+                    {
+                        Console.WriteLine("File not found");
+                        isNameValid = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (fileNameEnteredByAUser == "")
+                    {
+                        Console.WriteLine("File name cannot be empty.");
+                    }
+                    else if (fileNameEnteredByAUser == null)
+                    {
+                        Console.WriteLine("File name cannot be null.");
+                    }
+
+                    isNameValid = false;
+                    break;
+                }
+
+                if (isFileDataValid)
+                {
+                    isJsonFileNotEmpty = IsJsonFileNotEmpty();
+
+                    if (isJsonFileNotEmpty)
+                    {
+                        LoadGames();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No games are present in the input file.");
+                    }
+                }
+                else
+                {
+                    PrintInvalidFormatMessage();
+                }
+
+            } while (!isNameValid);
+        }
+    }
+}
